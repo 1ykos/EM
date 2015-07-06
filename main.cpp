9377edeb5392133716451c657fd22d6c521207fb
Wolfgang Brehm <wolfgang.brehm@gmail.com>
2015-07-06 16:13:32 +0200

    Something is wrong the algorithm does not converge for starting values that are close to each other

--- main.cpp
+++ main.cpp
@@ -115,71 +115,76 @@ const vector<array<double,3>> ex_max(
   for (auto it=data.begin(); it!=data.end(); ++it){
     mean_variance((*it)[0],(*it)[1],sumw,mean,M2);
   }
-  const double s = sqrt(variance(M2,sumw,data.size()));
+  const double v = variance(M2,sumw,data.size());
   vector<array<double,3>> model(n); // weight,mean,sigma
   // initialize the model with equal weights,
   // random points from the dataset as mean
   // and the square root of the over all variance
   for (size_t i=0; i!=n; ++i){
-    model[i]={{1.0/n,data[i][0],s/n}};
+    model[i]={{1.0/n,data[i][0],v/n}};
   }
   vector<array<double,5>> temp(n); // sumw,mean,M2,T,TS
   double change;
   do{
     change=0.0;
     double TS = 0.0;
+//     for (auto t=temp.begin(); t!=temp.end(); ++t){
+//       (*t)[0]=(*t)[1]=(*t)[2]=(*t)[3]=(*t)[4]=0.0;
+//     }
+//     for (double i=0; i<1.005; i+=0.01){
+//       double T = 0.0;
+//       cout << setw(16) << i ;
+//       auto t=temp.begin();
+//       auto m=model.begin();
+//       for (;m!=model.end()&&t!=temp.end(); (++m,++t)){
+//         const double m1= i;
+//         const double v1= 0.01*0.01;
+//         const double m2= (*m)[1];
+//         const double v2= (*m)[2];
+//         const double i = exp(-(m1-m2)*(m1-m2)/(2*(v1+v2)))/sqrt(2*pi*(v1+v2));
+//         T+=i;
+//         (*t)[3]=i;
+//         (*t)[4]+=i;
+//       }
+//       t=temp.begin();
+//       m=model.begin();
+//       for (;m!=model.end()&&t!=temp.end(); (++m,++t)){
+//         const double v = 0.01*0.01;
+//         const double w = (*t)[3]/(T*v);
+//         cout << setw(16) << round(w);
+//       }
+//       cout << endl;
+//     }
     for (auto t=temp.begin(); t!=temp.end(); ++t){
-      (*t)[4]=0.0;
+      (*t)[0]=(*t)[1]=(*t)[2]=(*t)[3]=(*t)[4]=0.0;
     }
-    for (auto d=data.begin(); d!=data.end(); ++d){
-      auto t=temp.begin();
-      auto m=model.begin();
-      for (;m!=model.end()&&t!=temp.end(); (++m,++t)){
-        const double s2=(*m)[2]*(*m)[2]+(*d)[1]*(*d)[1];
-        const double i =
-          exp(-((*d)[0]-(*m)[1])*((*d)[0]-(*m)[1])/(2.0*s2))/sqrt(2*s2*pi);
-        TS+=i;
-        (*t)[4]+=i;
-      }  
-    }  
-    for (auto t=temp.begin(); t!=temp.end(); ++t){
-      (*t)[0]=(*t)[1]=(*t)[2]=(*t)[3]=0.0;
-    }
-    {
-      auto t=temp.begin();
-      auto m=model.begin();
-      for (;m!=model.end()&&t!=temp.end(); (++m,++t)){
-        change+=((*m)[0]-(*t)[4]/TS)*((*m)[0]-(*t)[4]/TS);
-        (*m)[0]=(*t)[4]/TS;
-      }
-    }
+    cout << "--------------------------------" << endl;
     for (auto d=data.begin(); d!=data.end(); ++d){
       double T = 0.0;
       auto t=temp.begin();
       auto m=model.begin();
       for (;m!=model.end()&&t!=temp.end(); (++m,++t)){
-        const double s2=(*m)[2]*(*m)[2]+(*d)[1]*(*d)[1];
-        const double i =
-          exp(-((*d)[0]-(*m)[1])*((*d)[0]-(*m)[1])/(2.0*s2))/sqrt(2*s2*pi);
-        if (isnan(i)){
-          cout << (*d)[0] << " "
-               << (*m)[1] << " "
-               << (*m)[2] << " "
-               << (*d)[1] << endl;
-          cout << "NAAAAN !!! !!! " << endl;
-          exit(0);
-        }
+        const double s2=(*m)[2]+(*d)[1]*(*d)[1];
+        const double m1= (*d)[0];
+        const double v1= (*d)[1]*(*d)[1];
+        const double m2= (*m)[1];
+        const double v2= (*m)[2];
+        const double i = exp(-(m1-m2)*(m1-m2)/(2*(v1+v2)))/sqrt(2*pi*(v1+v2));
         T+=i;
+        TS+=i;
         (*t)[3]=i;
+        (*t)[4]+=i;
       }
       t=temp.begin();
       m=model.begin();
       if (T<numeric_limits<double>::epsilon()) continue;
       for (;m!=model.end()&&t!=temp.end(); (++m,++t)){
         if ((*t)[3]<numeric_limits<double>::epsilon()) continue;
-        const double x = (*d)[0];
-        const double w = (*t)[3]*(*m)[0]/(T*(*d)[1]*(*d)[1]);
-        mean_variance(x,w,(*t)[0],(*t)[1],(*t)[2]);
+        const double m = (*d)[0];
+        const double v = (*d)[1]*(*d)[1];
+        const double w = (*t)[3]/(T*v);
+//         cout << setw(16) << m << setw(16) << w << endl;
+        mean_variance(m,w,(*t)[0],(*t)[1],(*t)[2]);
       }
     }
     {
@@ -188,14 +193,16 @@ const vector<array<double,3>> ex_max(
       for (;m!=model.end()&&t!=temp.end(); (++m,++t)){
         change+=((*m)[0]-(*t)[4]/TS)*((*m)[0]-(*t)[4]/TS);
         change+=((*m)[1]-(*t)[1])*((*m)[1]-(*t)[1]);
-        change+=((*m)[2]-variance((*t)[2],(*t)[0]))
-               *((*m)[2]-variance((*t)[2],(*t)[0]));
+        change+=((*m)[2]-(*t)[2]/(*t)[0])
+               *((*m)[2]-(*t)[2]/(*t)[0]);
+        cout << setw(16) << (*m)[1] << setw(16) << (*m)[2] << endl;
         (*m)[0]=(*t)[4]/TS;
         (*m)[1]=(*t)[1];
-        (*m)[2]=variance((*t)[2],(*t)[0]);
+        (*m)[2]=(*t)[2]/(*t)[0];
       }
     }
-    cout << change << endl;
+    cout << "--------------------------------" << endl;
+//     cout << change << endl;
   }while(change>1e-24);
   return model;
 }
@@ -205,10 +212,10 @@ int main(int argc, char *argv[]){
   lognormal_distribution<double> lognormal;
   uniform_int_distribution<size_t> uniform(0,N-1);
   vector<array<double,2>> data;
-  for (size_t i=0; i<256*N; ++i){
+  for (size_t i=0; i<64*N; ++i){
     const double s = lognormal(urng);
-    data.push_back({normal_distribution<double>(uniform(urng),s)(urng),s});
-//     data.push_back({normal_distribution<double>(uniform(urng),0.5)(urng),0.1});
+//     data.push_back({normal_distribution<double>(uniform(urng),s)(urng),s});
+    data.push_back({normal_distribution<double>(uniform(urng),0.1)(urng),0.1});
   }
 //   for (auto it=data.begin(); it!=data.end(); ++it){
 //     cout << setw(16) << (*it)[0] << setw(16) << (*it)[1] << endl;
@@ -220,7 +227,7 @@ int main(int argc, char *argv[]){
   sort(model.begin(),model.end(),
        [](const array<double,3> &a, const array<double,3> &b){return a[1]<b[1];});
   for (auto it=model.begin(); it!=model.end(); ++it){
-    cout << setw(16) << (*it)[0] << setw(16) << (*it)[1] << setw(16) << (*it)[2] << endl;    
+    cout << setw(16) << (*it)[0] << setw(16) << (*it)[1] << setw(16) << sqrt((*it)[2]) << endl;    
   }
   return 0;  
 }
